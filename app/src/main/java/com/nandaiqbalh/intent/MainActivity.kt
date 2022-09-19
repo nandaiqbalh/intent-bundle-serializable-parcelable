c22e43fd1e14b06c3e8ba18c2ff08f74426ae8e9
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-09-19 23:25:19 +0700

    Implicit intent

--- app/src/main/java/com/nandaiqbalh/intent/MainActivity.kt
+++ app/src/main/java/com/nandaiqbalh/intent/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.nandaiqbalh.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity.apply
+import com.nandaiqbalh.intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnImplicit.setOnClickListener {
+            implicitIntent()
+        }
+    }
+
+    // implicit intent
+    private fun implicitIntent(){
+
+        val stringMessage = "Hi, there!"
+
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, stringMessage)
+            type = "text/plain"
+        }
+
+        if (sendIntent.resolveActivity(packageManager) != null){
+            startActivity(sendIntent)
+        }
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
